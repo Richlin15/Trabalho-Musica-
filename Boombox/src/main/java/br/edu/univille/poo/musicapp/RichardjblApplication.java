ae3302634d5327181215d79c6d25e29ab6d29553
Richlin15 <caua.richlin@univille.br>
2024-09-26 00:40:25 -0300

    Update and rename BoomboxApplication.java to RichardjblApplication.java

--- Boombox/src/main/java/br/edu/univille/poo/musicapp/RichardjblApplication.java
+++ Boombox/src/main/java/br/edu/univille/poo/musicapp/RichardjblApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoomboxApplication {
+public class RichardjblApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoomboxApplication.class, args);
+		SpringApplication.run(RichardjblApplication.class, args);
 	}
 
 }
